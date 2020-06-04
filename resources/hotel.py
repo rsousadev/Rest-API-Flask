a04a55cf841af118b504536c43a9087894417ef3
Ricardo Sousa <ricardo.dmsousa@gmail.com>
2020-06-03 21:25:52 -0300

    My First App REST API Using Flask

---
+++ resources/hotel.py
@@ -0,0 +1,61 @@
+from flask_restful import Resource, reqparse
+from models.hotel import HotelModel
+from flask_jwt_extended import jwt_required
+
+
+class Hoteis(Resource):
+    def get(self):
+        return {'hoteis': [hotel.json() for hotel in HotelModel.query.all()]}
+
+
+class Hotel(Resource):
+    atributos = reqparse.RequestParser()
+    atributos.add_argument('nome', type=str, required=True, help="The field 'nome' cannot left blank")
+    atributos.add_argument('estrelas', type=float, required=True, help="The field 'estrelas' cannot left blank")
+    atributos.add_argument('diaria')
+    atributos.add_argument('cidade')
+
+    def get(self, hotel_id):
+        hotel = HotelModel.find_hotel(hotel_id)
+        if hotel:
+            return hotel.json()
+        return {'message': 'Hotel not found.'}, 404
+
+    @jwt_required
+    def post(self, hotel_id):
+        if HotelModel.find_hotel(hotel_id):
+            return {'message': 'Hotel id {} already exists'.format(hotel_id)}, 400
+        dados = Hotel.atributos.parse_args()
+        hotel = HotelModel(hotel_id, **dados)
+        try:
+            hotel.save_hotel()
+        except Exception:
+            return{'message': 'An error occurred trying to save hotel.'}, 500
+        return hotel.json()
+
+    @jwt_required
+    def put(self, hotel_id):
+        dados = Hotel.atributos.parse_args()
+        hotel_encontrado = HotelModel.find_hotel(hotel_id)
+        if hotel_encontrado:
+            hotel_encontrado.update_hotel(**dados)
+            hotel_encontrado.save_hotel()
+            return hotel_encontrado.json(), 200
+        hotel = HotelModel(hotel_id, **dados)
+        try:
+            hotel.save_hotel()
+        except Exception:
+            return{'message': 'An error occurred trying to save hotel.'}, 500
+        return hotel.json(), 201
+
+    @jwt_required
+    def delete(self, hotel_id):
+        global hoteis
+        hotel = HotelModel.find_hotel(hotel_id)
+        if hotel:
+            try:
+                hotel.delete_hotel()
+            except Exception:
+                return {'message', 'An error occurred trying to delete'}
+            return {'message': 'Hotel deleted.'}
+        return {'message': 'Hotel not found'}, 404
